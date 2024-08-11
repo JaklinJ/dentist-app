5add7329b4ddbfdd91b54143a2adb9e2085bbb6f
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2024-08-11 16:54:16 +0300

    Worked on blog component. Made changes to treatments component and home component

--- drV/src/app/blog/blog.component.ts
+++ drV/src/app/blog/blog.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+// import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+// import {MatExpansionModule} from '@angular/material/expansion';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
+  // standalone: true,
+  // imports: [MatExpansionModule],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BlogComponent {
+  // readonly panelOpenState = signal(false);
 
 }
